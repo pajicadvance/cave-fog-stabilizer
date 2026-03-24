7cc2160c5b8520ff1d6e42d7ac1e04bc4082a247
pajic <32738446+pajicadvance@users.noreply.github.com>
2026-03-24 22:32:51 +0100

    Patch only once

--- src/main/java/me/pajic/modid/mixson/DataPatches.java
+++ src/main/java/me/pajic/modid/mixson/DataPatches.java
@@ -6,11 +6,17 @@ import net.ramixin.mixson.enums.DebugOption;
 
 public class DataPatches {
 
+	private static boolean initialized = false;
+
 	public static void init() {
-		if (ModTemplate.xplat().isDebug()) {
-			Mixson.enableDebugOption(DebugOption.BASIC_LOGGING);
-			Mixson.enableDebugOption(DebugOption.EXTRA_LOGGING);
-			Mixson.enableDebugOption(DebugOption.EXPORT_PATCHED_FILE);
+		if (!initialized) {
+			if (ModTemplate.xplat().isDebug()) {
+				Mixson.enableDebugOption(DebugOption.BASIC_LOGGING);
+				Mixson.enableDebugOption(DebugOption.EXTRA_LOGGING);
+				Mixson.enableDebugOption(DebugOption.EXPORT_PATCHED_FILE);
+			}
+
+			initialized = true;
 		}
 	}
 }
